6607e02273bf07936c32393f3d8f3995c5416d1d
1da0 <31944504+1da0@users.noreply.github.com>
2017-10-22 18:59:12 +0900

    Add files via upload

--- day13_hw.py
+++ day13_hw.py
@@ -1,34 +1,112 @@
-import turtle as t             # 거북이 입력
-import random
-t.shape("turtle")              # 거북이모양
+import turtle as t        # 거북이 입력
+t.shape("turtle")         # 거북이 모양
+ 
+t.up()                    # 선이 안나오도록 꼬리를 뗀다
+t.pos()              
+t.goto(-250, 250)         # 상자의 왼쪽 모서리로 보낸다
+t.down()                  # 선이 나타나도록 꼬리를 내린다
+t.goto(250, 250)          # 상자의 윗부분을 그린다
+t.goto(250, -250)         # 상자의 오른쪽부분을 그린다
+t.goto(-250, -250)        # 상자의 아랫부분을 그린다
+t.goto(-250, 250)         # 상자의 왼쪽부분을 그린다
+t.up()                    
+t.goto(0, 0)              # 공을 중앙으로 보낸다
+t.down()                   
 
-t.up()                         # 선이 안나오게 한다
-t.pos()                        
-t.goto(-250, 250)              # 왼쪽 모서리로 보낸다
-t.down()                       # 선을 그린다
-t.goto(250, 250)               # 상자의 윗부분
-t.goto(250, -250)              # 상자의 오른쪽 부분
-t.goto(-250, -250)             # 상자의 아랫부분
-t.goto(-250, 250)              # 상자의 왼쪽 부분  
-t.up()        
-t.goto(0, 0)                   # 상자의 중간으로 거북이를 보낸다
-t.down()                       # 선이 나오게 한다
+t.speed(5)                # 속도를 5로 정한다
+t.color("green")          # 공의 색깔을 초록색으로 정한다  
+t.shape("circle")         # 공모양으로 지정한다
 
-t.color("green")               # 공의 이동선의 색을 초록색으로 지정한다
-t.shape("circle")              # 공모양으로 바꾼다
-a = t.xcor()                   # 공의 x좌표를 a로 지정한다
-b = t.ycor()                   # 공의 y좌표를 b로 지정한다
-
-for x in range(100):           # 공튀기기를 100번 반복한다
-    n = random.randint(1, 360) # 공이 튀길 각도를 랜덤 n으로 지정한다
-    t.setheading(n)            # n 각도로 방향을 돌린다
-    t.fd(1)
+t.setheading(65)          # 각도를 65도로 정한다
+t.fd(1)                   # 앞으로 1만큼 간다
+t.pos()                   # 이동 후  좌표를 구한다
+a = t.xcor()              # 이동 후 x좌표를 a로 지정한다
+b = t.ycor()              # 이동 후 y좌표를 b로 지정한다
+while -250 < b < 250:     # 상자 밖으로 나가지 않도록 y좌표의 범위를 조건으로 정한다
+    t.forward(3)          # 범위 내에서 벽에 닿을 때까지 계속 앞으로 나아간다
     t.pos()
-    a = t.xcor()               # 이동 후 x좌표를 a로 지정한다
-    b = t.ycor()               # 이동 후 y좌표를 b로 지정한다
-    while -250 < a < 250:      # 상자 밖으로 나가지 않도록 x좌표의 범위를 조건으로 정한다
-        if -250 < b < 250:     # 마찬가지로 이동 가능한 y좌표의 범위를 정한다
-            t.forward(1)       # 범위 내에서 벽에 닿을 때까지 계속 앞으로 나아간다
-            t.pos()
-            a = t.xcor()
-            b = t.ycor()
+    a = t.xcor()
+    b = t.ycor()
+
+t.color("blue")           # 공의 색깔을 파랑색으로 정한다
+
+t.setheading(295)
+t.fd(1)
+t.pos()
+a = t.xcor()              # 이동 후 x좌표를 a로 지정한다
+b = t.ycor()              # 이동 후 y좌표를 b로 지정한다
+while -250 < a < 250:     # 상자 밖으로 나가지 않도록 x좌표의 범위를 조건으로 정한다
+    t.forward(3)          # 범위 내에서 벽에 닿을 때까지 계속 앞으로 나아간다
+    t.pos()
+    a = t.xcor()
+    b = t.ycor()
+
+t.speed(1)                # 공의 속도를 1로 변경한다
+
+t.setheading(245)
+t.fd(1)
+t.pos()
+a = t.xcor()              # 이동 후 x좌표를 a로 지정한다
+b = t.ycor()              # 이동 후 y좌표를 b로 지정한다
+while -250 < b < 250:     # 상자 밖으로 나가지 않도록 y좌표의 범위를 조건으로 정한다
+    t.forward(3)          # 범위 내에서 벽에 닿을 때까지 계속 앞으로 나아간다
+    t.pos()
+    a = t.xcor()
+    b = t.ycor()
+
+t.color("red")            # 공의 색깔을 빨강색으로 변경한다
+t.pensize(3)              # 굵기를 3으로 변경한다
+
+t.setheading(169)
+t.fd(1)
+t.pos()
+a = t.xcor()              # 이동 후 x좌표를 a로 지정한다
+b = t.ycor()              # 이동 후 y좌표를 b로 지정한다
+while -250 < a < 250:     # 상자 밖으로 나가지 않도록 x좌표의 범위를 조건으로 정한다
+    t.forward(3)          # 범위 내에서 벽에 닿을 때까지 계속 앞으로 나아간다
+    t.pos()
+    a = t.xcor()
+    b = t.ycor()
+
+t.speed(0)                # 공의 속도를 0으로 변경한다
+t.color("black")          # 공의 색깔을 검정색으로 변경한다
+
+t.setheading(45)
+t.fd(1)
+t.pos()
+a = t.xcor()              # 이동 후 x좌표를 a로 지정한다
+b = t.ycor()              # 이동 후 y좌표를 b로 지정한다
+while -250 < b < 250:     # 상자 밖으로 나가지 않도록 y좌표의 범위를 조건으로 정한다
+    t.forward(3)          # 범위 내에서 벽에 닿을 때까지 계속 앞으로 나아간다
+    t.pos()
+    a = t.xcor()
+    b = t.ycor()
+
+t.pensize(1)              # 굵기를 1로 변경한다
+
+t.setheading(315)
+t.fd(1)
+t.pos()
+a = t.xcor()              # 이동 후 x좌표를 a로 지정한다
+b = t.ycor()              # 이동 후 y좌표를 b로 지정한다
+while -250 < a < 250:     # 상자 밖으로 나가지 않도록 x좌표의 범위를 조건으로 정한다
+    t.forward(3)          # 범위 내에서 벽에 닿을 때까지 계속 앞으로 나아간다
+    t.pos()
+    a = t.xcor()
+    b = t.ycor()
+
+t.color("green")          # 공의 색깔을 초록색으로 변경한다
+t.pensize(3)              # 굵기를 3으로 변경한다
+
+t.setheading(190)
+t.fd(1)
+t.pos()
+a = t.xcor()              # 이동 후 x좌표를 a로 지정한다
+b = t.ycor()              # 이동 후 y좌표를 b로 지정한다
+while -250 < a < 250:     # 상자 밖으로 나가지 않도록 x좌표의 범위를 조건으로 정한다
+    t.forward(3)          # 범위 내에서 벽에 닿을 때까지 계속 앞으로 나아간다
+    t.pos()
+    a = t.xcor()
+    b = t.ycor()
+
+t.goto(0, 0)              # 상자의 중앙으로 공을 옮긴다
